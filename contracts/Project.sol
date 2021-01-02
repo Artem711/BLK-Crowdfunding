600568ce1fa5f748fecc7e6873897d510158f9a9
Artem Moshnin <tap.kap.tap@gmail.com>
2021-01-02 19:31:45 +0100

    project contract done

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.4;
 import "@openzeppelin/contracts/math/SafeMath.sol";
+ 
 
 contract Project {
     using SafeMath for uint256;
@@ -9,7 +10,7 @@ contract Project {
     enum State {
         Fundarising, 
         Expired,
-        Succesful
+        Successful
     }
 
     // State variables
@@ -26,7 +27,7 @@ contract Project {
     // Event that will be emitted whenever funding will be received
     event FundingRecieved(address contributor, uint amount, uint currentTotal);
     // Event that will be emitted whenever the project starter has received the funds
-    event CreatorRecieved(address recipient);
+    event CreatorRecievedFunds(address recipient);
     
     // Modifier to check current state
     modifier inState(State _state) {
@@ -66,12 +67,64 @@ contract Project {
 
     function checkIfFundingCompleteOrExpired() public {
         if (currentBalance >= goalAmount) {
-            state = State.Succesful;
+            state = State.Successful;
             payOut();
+        } else if (block.timestamp > timeDeadline) {
+            state  = State.Expired;
         }
+        completedAt = block.timestamp;
     } 
 
-    function payOut() internal inState(State.Succesful) returns(bool) {
+    // Function to give the received funds to project starter.
+    function payOut() internal inState(State.Successful) returns(bool) {
+        uint256 totalRaised = currentBalance;
+        currentBalance = 0;
 
+        if (creator.send(totalRaised)) {
+            emit CreatorRecievedFunds(creator);
+            return true;
+        } else {
+            currentBalance = totalRaised;
+            state = State.Successful;
+        }
+
+        return false;
+    }
+
+    // Function to retrieve donated amount when a project expires.
+    function getRefund() public inState(State.Expired) returns (bool) {
+        uint refundAmount = contributions[msg.sender];
+        require(refundAmount > 0);
+
+        contributions[msg.sender] = 0;
+        if (!msg.sender.send(refundAmount)) {
+            contributions[msg.sender] = refundAmount;
+            return false;
+        } else {
+            currentBalance = currentBalance.sub(refundAmount);
+        }
+
+        return true;
+    }
+
+ 
+    /** Function to get specific information about the project.
+    * Returns all the project's details */
+    function getDetails() public view returns  (
+        address payable _creator,
+        string memory _title,
+        string memory _description,
+        uint256 _timeDeadline,
+        State _state,
+        uint256 _currentBalance,
+        uint256 _goalAmount
+    ) {
+        _creator = creator;
+        _title = title;
+        _description = description;
+        _timeDeadline = timeDeadline;
+        _state = state;
+        _currentBalance = currentBalance;
+        _goalAmount = goalAmount;
     }
 }
