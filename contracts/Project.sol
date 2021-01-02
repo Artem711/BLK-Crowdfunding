c28b4cbe5fd1fea10917093ac11e5dec427df79f
Artem Moshnin <tap.kap.tap@gmail.com>
2021-01-02 19:11:03 +0100

    S

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -14,10 +14,10 @@ contract Project {
 
     // State variables
     address payable public creator;
-    uint public amountGoal;  // required to reach at least this much, else everyone gets refund
+    uint public goalAmount;  // required to reach at least this much, else everyone gets refund
     uint public completedAt;
     uint256 public currentBalance;
-    uint public raiseBy;
+    uint public timeDeadline;
     string public title;
     string public description;
     State public state = State.Fundarising;
@@ -40,5 +40,38 @@ contract Project {
         _;
     }
 
-    constructor() public {}
+    constructor(
+        address payable _creator,
+        string memory _title,
+        string memory _description,
+        uint _timeDeadline,
+        uint _goalAmount
+    ) {
+        creator = _creator;
+        title = _title;
+        description = _description;
+        goalAmount = _goalAmount;
+        timeDeadline = _timeDeadline;
+        currentBalance = 0;
+    }
+
+    // @dev Function to fund a certain project.
+    function contribute() external inState(State.Fundarising) payable {
+        require(msg.sender != creator);
+        contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+        currentBalance = currentBalance.add(msg.value);
+        emit FundingRecieved(msg.sender, msg.value, currentBalance);
+        checkIfFundingCompleteOrExpired();
+    }
+
+    function checkIfFundingCompleteOrExpired() public {
+        if (currentBalance >= goalAmount) {
+            state = State.Succesful;
+            payOut();
+        }
+    } 
+
+    function payOut() internal inState(State.Succesful) returns(bool) {
+
+    }
 }
